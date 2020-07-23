76f509277218b1a326658cd972e3dddad6730ca1
Khanecz <Michal.rezabek@gmail.com>
2020-07-23 14:09:25 +0200

    Functionality

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,6 @@
 const apiKey = "5B3dyUmyimGAwUznqRpbpoQ0ogTKWKTo2_xhdEeQjskxRmcN_0Eo3dje9pn8pGsQeRtb6ij_dZHyPvLWHsMmpuWL-XmyPMYg-Dk-i9Ijo6YjtgiPb6R-ERxTY1AZX3Yx";
 
-export const Yelp= {
+const Yelp= {
     search(term,location,sortBy) {
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
          {headers: { Authorization : `Bearer ${apiKey}`}}).then(response => {
@@ -12,11 +12,11 @@ export const Yelp= {
                         id: business.id,
                         imageSrc: business.image_url,
                         name: business.name,
-                        address: business.address1,
-                        city: business.city,
-                        state: business.state,
-                        zipCode: business.zip_code,
-                        category: business.categories.title,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
                         rating: business.rating,
                         reviewCount: business.review_count
                     }
@@ -26,5 +26,5 @@ export const Yelp= {
     }
 };
 
-
+export default Yelp;
 
